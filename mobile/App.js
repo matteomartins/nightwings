15311e1a586439d22764209ae651985be71891a7
matteomartins <matteomcamargo@gmail.com>
2020-03-26 21:09:18 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
